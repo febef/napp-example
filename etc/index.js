e35ff15cc78e9934e6fa2e7ea8354ed25c058010
febef - Facundo Beltramo <febef.dev@gmail.com>
2019-01-31 19:25:19 -0300

    [+] example.

--- etc/index.js
+++ etc/index.js
@@ -1,9 +1,11 @@
 
-const fs = require('fs');
-
+const path = require('path');
+const fs   = require('fs');
+const basedir = path.join(__dirname ,process.env.ENV || 'dev');
 fs
-  .readdirSync(__dirname)
+  .readdirSync(basedir)
   .filter(file => file.slice(-4) == 'json')
   .forEach(file => {
-    module.exports[file.slice(0,-5)] = require(__dirname + '/' + file);
+    module.exports[file.slice(0,-5)] =
+      require(path.join(basedir, file));
 })
